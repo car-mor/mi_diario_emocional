a6537c8525b3bce15a86276cc5ec44d6316bed53
Carlos Moreno <cmorenoh2000@alumno.ipn.mx>
2025-11-26 17:34:27 -0600

    Cambios en WomanHeart

--- frontend/src/modules/auth/components/WomanHeart.vue
+++ frontend/src/modules/auth/components/WomanHeart.vue
@@ -3,7 +3,7 @@
     <img
       src="/images/mujer_corazon.jpg"
       alt="Mujer con corazón"
-      class="w-full h-full object-cover dark:border-gray-700"
+      class="mx-auto object-cover w-56 sm:w-72 md:w-80 lg:w-[420px] h-56 sm:h-72 md:h-80 lg:h-[420px] rounded-2xl border border-gray-200 dark:border-gray-700 shadow-md transition-transform duration-300 hover:scale-105"
     />
   </div>
 </template>
